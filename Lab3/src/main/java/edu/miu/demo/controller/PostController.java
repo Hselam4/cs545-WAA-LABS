d7ae873879382cbb213762bf3aa1b8bb6d1111de
selam <selamawithabte@miu.edu>
2022-11-17 21:18:35 -0600

    updated

--- Lab3/src/main/java/edu/miu/demo/controller/PostController.java
+++ Lab3/src/main/java/edu/miu/demo/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/posts")
 public class PostController {
     @Autowired
